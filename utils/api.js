674303807020c257a60bd92deb94737ace95ec2b
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-06-11 21:57:44 +0500

    feat: add api file with asyncstorage helpers

---
+++ utils/api.js
@@ -0,0 +1,33 @@
+import { AsyncStorage } from 'react-native'
+const DECKS_STORAGE_KEY = 'armflashcards:decks'
+
+export function getDecks () {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(JSON.parse)
+}
+
+export function getDeck (title) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      return data[title]
+    })
+}
+
+export function saveDeckTitle (title) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+    [title]: {
+      title,
+      questions: []
+    }
+  }))
+}
+
+export function addCardToDeck (title, card) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      const deck = data[title]
+      deck.questions = deck.questions.concat([card])
+      return AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
+    })
+}
