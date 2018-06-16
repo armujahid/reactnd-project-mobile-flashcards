426cb6325eefbfba500951fc3a4c80e8c7b882bb
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-06-16 21:39:01 +0500

    feat: update NewDeck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,12 +1,43 @@
 import React, { PureComponent } from 'react'
-import { StyleSheet, Text, View, Platform, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import TextButton from './TextButton'
+import { connect } from 'react-redux';
+import { addDeck } from '../actions'
+class NewDeck extends PureComponent {
+  state = {
+    title: ''
+  }
 
-export default class NewDeck extends PureComponent {
+  handleSubmit = () => {
+    const { submitTitle, goBack } = this.props
+    submitTitle(this.state.title)
+    this.setState({title: ''})
+    goBack()
+  }
   render() {
+    const { title } = this.state
+
     return (
       <View>
-        <Text>New Deck</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Deck Title"
+          value={title}
+          onChangeText={(title) => this.setState({title})}
+        />
+        <TextButton style={{margin: 20}} onPress={this.handleSubmit} >
+          Submit
+        </TextButton>
       </View>
     )
   }
 }
+
+const mapDispatchToProps = (dispatch, { navigation }) => {
+  return {
+    submitTitle: (title) => dispatch(addDeck(title)),
+    goBack: () => navigation.goBack(),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(NewDeck)
