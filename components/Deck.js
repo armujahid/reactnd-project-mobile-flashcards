e4735095a2072a05247da23349d345f9f27597be
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-06-19 22:41:21 +0500

    fix: show 1 card instead of 1 cards

--- components/Deck.js
+++ components/Deck.js
@@ -4,10 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class Deck extends PureComponent {
   render() {
     const { deck } = this.props
+    const noOfCards = deck.questions.length
     return (
         <View>
           <Text style={[styles.text, styles.header]}>{deck.title}</Text>
-          <Text style={styles.text}>{deck.questions.length} cards</Text>
+          <Text style={styles.text}>{noOfCards} {noOfCards === 1? 'card' : 'cards'}</Text>
         </View>
     )
   }
