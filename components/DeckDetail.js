a1f5e90eb6bf200af36f738ae43b90d37be49795
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-06-20 21:13:20 +0500

    style: DeckDetails

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
-import { white } from '../utils/colors'
+import { white, purple } from '../utils/colors'
 import TextButton from './TextButton'
 import Deck from './Deck'
 import styles from '../styles'
@@ -34,13 +34,16 @@ class DeckDetail extends Component {
     return (
       <View style={styles.container}>
         <Deck deck={deck}/>
-        <TextButton style={{margin: 20}} onPress={() => this.props.navigation.navigate(
+        <TextButton
+          textColor={purple}
+          backgroundColor={white}
+          onPress={() => this.props.navigation.navigate(
           'NewCard',
           { deckTitle: deck.title }
         )}>
           Add Card
         </TextButton>
-        <TextButton style={{margin: 20}}
+        <TextButton
           disabled={deck.questions.length === 0}
           onPress={this.startQuiz}>
           Start Quiz
